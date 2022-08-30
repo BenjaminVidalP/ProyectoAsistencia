a5961cae8ea8cce454eef0f74543cf81a439460a
BenjaminVidalP <be.vidalp@duocuc.cl>
2022-08-30 19:41:18 -0400

    arreglo qr 2.2

--- ProyectoAsistencia/src/app/pages/registrar/registrar.page.ts
+++ ProyectoAsistencia/src/app/pages/registrar/registrar.page.ts
@@ -8,7 +8,8 @@ import { MenuController } from '@ionic/angular';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor() {
+  constructor(private menu: MenuController) {
+    this.menu.enable(false);
     
    }
 
